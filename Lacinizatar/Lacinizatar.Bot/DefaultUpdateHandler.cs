fb61efae940febd8f073ec6aeb3eee7da32aeef9
Vladislav Kochegin <kocheginvladislav@gmail.com>
2022-09-16 12:12:01 +0200

    Add interaction with bot

---
+++ Lacinizatar/Lacinizatar.Bot/DefaultUpdateHandler.cs
@@ -0,0 +1,42 @@
+﻿using Lacinizatar.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+namespace Lacinizatar.Bot
+{
+    internal class DefaultUpdateHandler : IUpdateHandler
+    {
+        public Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            var message = update.Message;
+            if (message == null)
+            {
+                return;
+            }
+
+            var text = message.Text;
+            if (text == null || text == string.Empty)
+            {
+                return;
+            }
+
+            if (text.Any(l => !ToBelLatin.belLetters.Contains(char.ToLowerInvariant(l)) && char.IsLetter(l)))
+            {
+                return;
+            }
+
+            await botClient.SendTextMessageAsync(message.Chat.Id, new ToBelLatin(text).Translate(), cancellationToken: cancellationToken);
+        }
+    }
+}
--- Lacinizatar/Lacinizatar.Bot/Program.cs
+++ Lacinizatar/Lacinizatar.Bot/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Lacinizatar.Bot;
 using Lacinizatar.Logic;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
+using var cts = new CancellationTokenSource();
+var bot = new TelegramBotClient("5657259321:AAEOK2pfS6OaKJeb5xa5wW430fak-arSvYo");
+bot.StartReceiving(new Lacinizatar.Bot.DefaultUpdateHandler(), new ReceiverOptions() { AllowedUpdates = Array.Empty<UpdateType>() });
+Console.ReadLine();
+cts.Cancel();
